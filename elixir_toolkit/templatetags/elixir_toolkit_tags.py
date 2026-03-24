350cc7f214635206fd528e4d73eb1dedc55369a9
Ehcraball <nathan95130.nl@gmail.com>
2026-03-24 17:00:20 +0100

    add : selectiz with and without context choices

--- elixir_toolkit/templatetags/elixir_toolkit_tags.py
+++ elixir_toolkit/templatetags/elixir_toolkit_tags.py
@@ -3,6 +3,7 @@ from django import template
 from django.utils.safestring import mark_safe
 from django.templatetags.static import static
 from django.forms.utils import flatatt
+import ast
 
 register = template.Library()
 
@@ -29,3 +30,25 @@ def ui_button(text, css_classes="", icon=None, icon_right=False, href=None, **kw
 def ui_button_primary(text, css_classes="", **kwargs):
     new_classes = f"is-primary {css_classes}".strip()
     return ui_button(text, css_classes=new_classes, **kwargs)
+
+
+@register.inclusion_tag('elixir_toolkit/components/select.html')
+def ui_select(name, options, element_id=None, selected=None, placeholder="Choisissez...", icon=None, css_classes=""):
+    if isinstance(options, str):
+        try:
+            options = ast.literal_eval(options)
+        except (ValueError, SyntaxError):
+            options = []
+
+    if isinstance(options, dict):
+        options = options.items()
+
+    return {
+        'name': name,
+        'element_id': element_id or f"id_{name}",
+        'options': options,
+        'selected': str(selected) if selected else None,
+        'placeholder': placeholder,
+        'icon': icon,
+        'css_classes': css_classes,
+    }
--- test_views/views.py
+++ test_views/views.py
@@ -8,9 +8,21 @@ from test_views.forms import FormExample
 #     template_name = "home.html"
 
 
+
 class FormTestView(FormView):
     template_name = "form_test.html"
     form_class = FormExample
 
-    # def form_valid(self, form):
-    #     return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['mes_options_paiement'] = [
+            ('SCHE', 'Paiement échéance'),
+            ('FREE', 'Montant libre'),
+            ('CANC', 'Annulé'),
+        ]
+        
+        # Tu peux même envoyer une valeur sélectionnée par défaut
+        context['mode_par_defaut'] = 'SCHE'
+        
+        return context
