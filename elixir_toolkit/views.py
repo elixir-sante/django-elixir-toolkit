e8c2b3f772314ae600018cb97cd9a7b060c7da27
gaeldb <barbier.gael@free.fr>
2026-05-19 14:00:05 +0200

    Implement AsyncFormView for async form handling
    Added AsyncFormView to handle asynchronous form processing.

--- elixir_toolkit/views.py
+++ elixir_toolkit/views.py
@@ -1,3 +1,22 @@
+from django.views.generic import FormView
 from django.shortcuts import render
 
-# Create your views here.
+
+class AsyncFormView(FormView):
+    """Ceci est une réécriture async de la ProcessFormView de Django
+    pour permettre d'utiliser des vues basées sur des formulaires avec
+    des appels asynchrones (ex: httpx).
+    Voir le fichier django/views/generic/edit.py pour la version synchrone d'origine."""
+
+    async def get(self, request, *args, **kwargs):
+        return self.render_to_response(self.get_context_data())
+    
+    async def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return await self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    async def put(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
