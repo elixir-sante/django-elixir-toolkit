058b81fe23a7200b55fb7830caafa8c76359a7e7
gaeldb <barbier.gael@free.fr>
2026-05-20 16:04:44 +0200

    Implement MixinDispatch for pre-dispatch handling
    Add MixinDispatch class to handle pre-dispatch logic.

--- elixir_toolkit/mixins.py
+++ elixir_toolkit/mixins.py
@@ -2,13 +2,25 @@ from asgiref.sync import sync_to_async
 from django.core.exceptions import ImproperlyConfigured
 
 
+class MixinDispatch:
+    """
+    Mixin to ensure that the pre_dispatch method is called before the main dispatch method.
+    This allows us to load necessary data for the context before handling the request.
+    """
+    def pre_dispatch(self, request, *args, **kwargs):
+        """Méthode appelée avant le dispatch pour charger les données nécessaires au contexte."""
+        pass
+
+
 class AsyncMixinDispatch:
+
     async def dispatch(self, request, *args, **kwargs):
         await sync_to_async(self.pre_dispatch)(request, *args, **kwargs) # type: ignore
         return await super().dispatch(request, *args, **kwargs) # type: ignore
 
 
 class SyncMixinDispatch:
+
     def dispatch(self, request, *args, **kwargs):
         if not hasattr(self, 'view_is_async'):
             raise ImproperlyConfigured(
