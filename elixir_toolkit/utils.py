e7c3d4f54dceb2b3169a635b8b976147619f4e83
Samito-05 <procoppesam@gmail.com>
2026-05-22 15:57:29 +0200

    fix: import * implementation

--- elixir_toolkit/utils.py
+++ elixir_toolkit/utils.py
@@ -1,5 +1,15 @@
 from asgiref.sync import sync_to_async
 
+__all__ = [
+    "session_get",
+    "session_aget",
+    "session_set",
+    "session_aset",
+    "session_pop",
+    "session_apop",
+]
+
+
 @sync_to_async
 def session_aget(request, key, default=None):
     return request.session.get(key, default)
@@ -11,16 +21,20 @@ def session_get(request, key, default=None):
 def session_aset(request, **kwargs):
     for k, v in kwargs.items():
         request.session[k] = v
+    request.session.modified = True
 
 def session_set(request, **kwargs):
     for k, v in kwargs.items():
         request.session[k] = v
+    request.session.modified = True
 
 @sync_to_async
 def session_apop(request, *keys):
     for k in keys:
         request.session.pop(k, None)
+    request.session.modified = True
 
 def session_pop(request, *keys):
     for k in keys:
         request.session.pop(k, None)
+    request.session.modified = True
