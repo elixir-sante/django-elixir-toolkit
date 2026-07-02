a0bf8f2c88efceffad5a84a4ba0e56e2d4d26e43
ehcraball <nathan.labarche@gmail.com>
2026-07-02 16:37:29 +0200

    add : captcha

---
+++ elixir_toolkit/templatetags/liveidentity_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.inclusion_tag('elixir_front/captcha/liveidentity_script.html')
+def render_captcha_script(form_id):
+    return {
+        'sp_key': getattr(settings, 'LIVEIDENTITY_SP_KEY', ''),
+        'form_id': form_id
+    }
--- elixir_toolkit/utils.py
+++ elixir_toolkit/utils.py
@@ -1,4 +1,12 @@
 from asgiref.sync import sync_to_async
+import base64
+import logging
+from django.conf import settings
+from django.http import HttpRequest
+import httpx
+
+logger = logging.getLogger(__name__)
+
 
 __all__ = [
     "session_get",
@@ -38,3 +46,51 @@ def session_pop(request, *keys):
     for k in keys:
         request.session.pop(k, None)
     request.session.modified = True
+
+
+async def validate_liveidentity_token(request: HttpRequest) -> bool:
+    """
+    Valide de manière asynchrone le token LiveIdentity extrait de la requête.
+    Centralisé dans le toolkit.
+    """
+    # Extraction centralisée : si le nom du champ change, on le modifie uniquement ici
+    token = request.POST.get("liveidentity_token")
+    
+    if not token or token == "100":
+        logger.warning("[LiveIdentity] Validation abandonnée : token manquant ou erreur front (code 100).")
+        return False
+        
+    url = "https://captcha.liveidentity.com/captcha/public/backend/api/v4/captcha/check"
+    
+    client_id = getattr(settings, 'LIVEIDENTITY_CLIENT_ID', None)
+    client_secret = getattr(settings, 'LIVEIDENTITY_SECRET', None)
+    
+    if not client_id or not client_secret:
+        logger.error("[LiveIdentity] Configuration manquante (CLIENT_ID ou SECRET) dans les settings.")
+        return False
+
+    # Encodage Basic Auth
+    raw_credentials = f"{client_id.strip()}:{client_secret.strip()}"
+    base64_credentials = base64.b64encode(raw_credentials.encode('utf-8')).decode('utf-8')
+    
+    headers = {
+        "authorization": f"Basic {base64_credentials}",
+        "content-type": "application/json"
+    }
+    
+    payload = {
+        "antibotToken": token
+    }
+    
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url, json=payload, headers=headers, timeout=5.0)
+            if response.status_code == 200:
+                result = response.json()
+                return result.get("result") == "SUCCESS"
+                
+            logger.error(f"[LiveIdentity Error {response.status_code}] Réponse Orange : {response.text}")
+        except httpx.RequestError as exc:
+            logger.error(f"[LiveIdentity Network Error] Échec de la requête : {exc}")
+            
+    return False
