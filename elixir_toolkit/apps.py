4c4c82b84721e5c3f752c804b7dd3f1fa55b5b99
Samito-05 <procoppesam@gmail.com>
2026-07-07 17:41:49 +0200

    feat : auto-inject default settings via AppConfig

--- elixir_toolkit/apps.py
+++ elixir_toolkit/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ElixirToolkitConfig(AppConfig):
     name = 'elixir_toolkit'
+
+    def ready(self):
+        from . import defaults
+        for name in dir(defaults):
+            if name.isupper() and not name.startswith('_'):
+                if not hasattr(settings, name):
+                    setattr(settings, name, getattr(defaults, name))
