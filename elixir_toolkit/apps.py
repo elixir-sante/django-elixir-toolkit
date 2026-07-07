edd8f578be877e89a2ce1a5305564028437e6af7
Samito-05 <procoppesam@gmail.com>
2026-07-07 18:13:13 +0200

    fix: timing on settings

--- elixir_toolkit/apps.py
+++ elixir_toolkit/apps.py
@@ -1,13 +1,18 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+from . import defaults
+
+
+def _inject_defaults():
+    for name in dir(defaults):
+        if name.isupper() and not name.startswith('_'):
+            if not hasattr(settings, name):
+                setattr(settings, name, getattr(defaults, name))
+
+
+_inject_defaults()
+
 
 class ElixirToolkitConfig(AppConfig):
     name = 'elixir_toolkit'
-
-    def ready(self):
-        from . import defaults
-        for name in dir(defaults):
-            if name.isupper() and not name.startswith('_'):
-                if not hasattr(settings, name):
-                    setattr(settings, name, getattr(defaults, name))
