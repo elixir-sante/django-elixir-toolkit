aa3a9ff5d94dd9e98906a40deec63e5f3ec5ba40
Gaël D. Barbier <gael@elixir-sante.fr>
2026-09-25 11:25:24 +0200

    feat: add async form_invalid method to AsyncFormView
    - Add await to form_invalid call in post method
    - Implement async form_invalid method with proper context handling

--- elixir_toolkit/views.py
+++ elixir_toolkit/views.py
@@ -17,7 +17,11 @@ class AsyncFormView(FormView):
         if form.is_valid():
             return await self.form_valid(form)
         else:
-            return self.form_invalid(form)
+            return await self.form_invalid(form)
+
+    async def form_invalid(self, form):
+        context = await self.get_context_data(form=form)
+        return self.render_to_response(context)
 
     async def put(self, *args, **kwargs):
         return self.post(*args, **kwargs)
